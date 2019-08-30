94919df6f0c013fec87c6511b876be95dddeae81
Oscar Zhang <tzhang383@wisc.edu>
2019-08-30 14:02:46 -0300

    functionized resize.py

--- resize.py
+++ resize.py
@@ -2,6 +2,20 @@ from PIL import Image
 
 from resizeimage import resizeimage
 
+
+def resize_style(name, dimension=[400,300]):
+    with open('style/raw/'+name, 'rb') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_cover(image, dimension)
+            cover.save('style/resize/'+name.split('.')[0]+'_resize.'+name.split('.')[1], image.format)
+
+def resize_content(name, dimension=[400,300]):
+    with open('content/raw/'+name, 'rb') as f:
+        with Image.open(f) as image:
+            cover = resizeimage.resize_cover(image, dimension)
+            cover.save('content/resize/'+name.split('.')[0]+'_resize.'+name.split('.')[1], image.format)
+        
+
 with open('style/raw/bright.jpg', 'rb') as f:
     with Image.open(f) as image:
         cover = resizeimage.resize_cover(image, [400, 300])
